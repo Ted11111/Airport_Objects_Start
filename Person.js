5a6d947631fb0e310baa192383dcd0bb89e06b63
Ted1111 <teddyslight@gmail.com>
2023-02-08 11:57:45 +0000

    Completed Person.js

--- Person.js
+++ Person.js
@@ -1,10 +1,22 @@
+
+const Bag = require("./Bag")
+
 class Person {
     // Include a Constructor Method for Properties: 
         // 1. Name
         // 2. Bags
             // 2a. Bags should contain/reference a data type for storing more than one bag object.
-
+    constructor(name){
+        this.name = name
+        this.bags = []
+    }
     // Include an addBag method
+    addBag() {
+        this.bags.push(Bag);
+    }
+
 }
 
 //Export the Module
+
+module.exports = Person
